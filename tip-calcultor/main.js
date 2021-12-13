0eb0868ce8fb05f3577b0957f8a84cda14c45a75
Gopal Jajam <61733665+Gopaljajam@users.noreply.github.com>
2021-12-13 10:29:44 +0530

    Add files via upload

---
+++ tip-calcultor/main.js
@@ -0,0 +1,43 @@
+// selecting the inputs btns and show calculation
+const billAmount = document.querySelector("#bill-amount");
+const inputNOP = document.querySelector("#input-nop");
+const tipPercent = document.querySelectorAll(".tip-per");
+const manualTip = document.querySelector('#manual-tip');
+const nan = document.querySelector('.nan');
+const tipAmountShow = document.querySelector("#tip-amount");
+const totalPeopleShow = document.querySelector("#total-peo");
+const resetBtn = document.querySelector("#reset-btn");
+
+
+tipPercent.forEach((tipPer) => {
+    tipPer.addEventListener("click", tipPerFunc);
+});
+
+manualTip.addEventListener("input", tipPerFunc);
+
+resetBtn.addEventListener('click', () => {
+    tipAmountShow.innerHTML = "0.00";
+    totalPeopleShow.innerHTML = "0.00"
+})
+
+function tipPerFunc() {
+    this.classList.toggle("active-class")
+    if (this.value) {
+        let bill = billAmount.value;
+        const totalTip = bill * this.value / 100;
+        inputNOP.addEventListener("input", () => {
+            if (inputNOP.value == 0 || inputNOP.value == null) {
+                tipAmountShow.innerHTML = "0.00";
+            } else if (inputNOP.value) {
+                const tip = totalTip / inputNOP.value;
+                tipAmountShow.innerHTML = tip.toString().split("").splice(0, 4).join("");
+            }
+            if (inputNOP.value == 0 || inputNOP.value == null) {
+                totalPeopleShow.innerHTML = "0.00";
+            } else if (inputNOP.value) {
+                const totalPer = bill / inputNOP.value;
+                totalPeopleShow.innerHTML = totalPer.toString().split("").splice(0, 4).join("")
+            }
+        });
+    }
+}
